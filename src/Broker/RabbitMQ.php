74f329823a753e662cf9e776a544f0925402d546
Clivern <hello@clivern.com>
2020-03-15 21:17:19 +0100

    update

---
+++ src/Broker/RabbitMQ.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Chunk - Asynchronous Task Queue Based on Distributed Message Passing for PHP
+ * (c) Clivern <hello@clivern.com>
+ */
+
+namespace Clivern\Chunk\Broker;
+
+use Clivern\Chunk\Contract\BrokerContract;
+
+/**
+ * RabbitMQ Class.
+ */
+class RabbitMQ implements BrokerContract
+{
+}
---
+++ src/Contract/BrokerContract.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Chunk - Asynchronous Task Queue Based on Distributed Message Passing for PHP
+ * (c) Clivern <hello@clivern.com>
+ */
+
+namespace Clivern\Chunk\Contract;
+
+/**
+ * Broker Contract.
+ */
+interface BrokerContract
+{
+}
